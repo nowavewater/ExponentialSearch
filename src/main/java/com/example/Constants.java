df0faef372ce60c35259871fb791c0bdca6575f4
Pengcheng Yin <nowavewater@gmail.com>
2016-05-11 18:08:36 -0700

    Update Constants.java

--- src/main/java/com/example/Constants.java
+++ src/main/java/com/example/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	public static final String PATH = "/files/"; 
 	public static final String FILE_NAME = "demo.txt";
-	public static final long num1 = 2;
+	public static final long num1 = 1;
 	public static final long num2 = 10000000;
 	
 	public static final long BYTES_OF_LINE = 55;
